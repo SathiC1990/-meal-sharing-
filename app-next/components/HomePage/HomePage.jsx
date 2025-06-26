dbd9169e84a368f1df1e8358a9d6d831da9ccf3a
sathic <sathichowdhury2017@gmail.com>
2025-06-26 11:55:46 +0200

    React2-Week1

--- app-next/components/HomePage/HomePage.jsx
+++ app-next/components/HomePage/HomePage.jsx
@@ -1,19 +1,16 @@
 import HYFLogo from "@/assets/hyf.svg";
 import Image from "next/image";
 import "./HomePage.css";
+import MealsList from "@/components/MealsList/MealsList";
 
-// Feel free to replace the content of this component with your own
-function HomePage() {
+const HomePage = () => {
   return (
-    <>
-      <a href="https://www.hackyourfuture.dk/" target="_blank" className="link">
-        <Image src={HYFLogo.src} width={HYFLogo.width} height={HYFLogo.height} className="logo" />
-      </a>
-      <a href="/nested" className="link">
-        <span className="message">Go to the nested page</span>
-      </a>
-    </>
+    <main>
+      <h1 className="welcomeTitle">Welcome to Meal Sharing</h1>
+
+      <MealsList /> 
+    </main>
   );
-}
+};
 
 export default HomePage;
